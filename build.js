6cc479ff6076812eb356a3221dccfb018aa19e58
David Laribee <laribee@gmail.com>
2017-03-24 08:03:26 -0400

    Fix $module_dir variable name.

--- build.js
+++ build.js
@@ -4,24 +4,27 @@ const fs = require('fs')
 const path = require('path')
 const process = require('process')
 
+console.log(process.argv1)
 const workshopPath = './workshops/example' // TODO: Make an argument
 
 const isSlide = (module) => module.endsWith('.slide')
 
 const readFile = (file) => fs.readFileSync(file).toString()
 
+const slides = []
+
 const readManifest = (moduleDir, basePath = process.env.NN_WORKSHOP_MODULES) => {
   const manifestPath = path.join(moduleDir, '.module')
-  const entries = readFile(manifestPath).split('\n').filter((element) => element !== '');
+  const entries = readFile(manifestPath).split('\n').filter((element) => element !== '')
 
   for (entry of entries) {
     if (isSlide(entry)) {
       console.log(`SLIDE -> ${entry}`)
-      console.log(readFile(path.join(basePath, entry)))
+      const slide = readFile(path.join(basePath, entry)).replace('$module_dir', path.relative(workshopPath, basePath))
+      slides.push(slide)
     } else {
-      console.log(`MODULE -> ${entry}`)
       const modulePath = path.join(process.env.NN_WORKSHOP_MODULES, entry)
-      console.log(`PATH -> ${modulePath}`)
+      console.log(`MODULE -> ${modulePath}`)
       readManifest(modulePath, modulePath)
     }
   }
@@ -29,3 +32,5 @@ const readManifest = (moduleDir, basePath = process.env.NN_WORKSHOP_MODULES) => 
 
 
 readManifest(workshopPath, workshopPath)
+
+console.log(slides.join('\n'))
