7e413188d5f3def91ad5e14d74d46a58dcdc2931
David Laribee <laribee@gmail.com>
2017-04-02 15:58:23 -0400

    Simplify example module.

--- test/module.js
+++ test/module.js
@@ -1,49 +1,44 @@
-const chai = require("chai")
+const chai = require('chai')
 const expect = chai.expect
 chai.use(require('chai-string'))
-const path = require("path")
+const path = require('path')
 const loader = require('../lib/loader')
 
 const Module = require('../lib/module')
 
-describe("Module", () => {
-
-  describe("Creating", () => {
-
-    this.subject = new Module(path.join(loader.moduleRepo(), "example-module"))
+describe('Module', () => {
+  describe('Creating', () => {
+    this.subject = new Module(path.join(loader.moduleRepo(), 'example-module'))
 
     it("assumes modules are in a subdir 'modules'", () => {
-      expect(this.subject.manifestPath).to.startWith(path.join(process.cwd(), "modules"))
+      expect(this.subject.manifestPath).to.startWith(path.join(process.cwd(), 'modules'))
     })
 
     it("appends 'module.yml' to the path", () => {
-      expect(this.subject.manifestPath).to.endWith("example-module/module.yml")
+      expect(this.subject.manifestPath).to.endWith('example-module/module.yml')
     })
 
-    it("copies module data to the slide entry.", () => {
+    it('copies module data to the slide entry.', () => {
       expect(this.subject.sections[0].module.title).to.equal('Git/GitHub Best Practices')
       expect(this.subject.sections[10].module.title).to.equal('Test Sub Sub Module')
     })
 
-    it("copied module data DOES NOT contain sections", () => {
-      expect(this.subject.sections[0].module.sections).to.be.undefined;
+    it('copied module data DOES NOT contain sections', () => {
+      expect(this.subject.sections[0].module.sections).to.be.undefined
     })
 
-    describe("is done on the defense!", () => {
-
-      it("Throw an error when the module path does not exist.", () => {
-        let bogusPath = path.join(loader.moduleRepo(), "example2-module");
-        expect(() => new Module(bogusPath).to.throw("Module not found."))
+    describe('is done on the defense!', () => {
+      it('Throw an error when the module path does not exist.', () => {
+        let bogusPath = path.join(loader.moduleRepo(), 'example2-module')
+        expect(() => new Module(bogusPath).to.throw('Module not found.'))
       })
-
     })
-
   })
 
-  describe("Sections", () => {
-    it ("pulls in sections from referenced modules", () => {
-      const subject = new Module(path.join(loader.moduleRepo(), "example-module"))
-      expect(subject.sections.length).to.equal(17)
+  describe('Sections', () => {
+    it('pulls in sections from referenced modules', () => {
+      const subject = new Module(path.join(loader.moduleRepo(), 'example-module'))
+      expect(subject.sections.length).to.equal(16)
     })
   })
 })
