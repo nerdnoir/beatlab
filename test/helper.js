31fbb5758259f98e47850f889fc860fb48f86642
David Laribee <laribee@gmail.com>
2017-04-05 12:33:39 -0400

    Modify test helper.js

--- test/helper.js
+++ test/helper.js
@@ -1,4 +1,16 @@
-const {describe, it} = require('mocha')
+// eslint-disable-next-line no-unused-vars
+const mocha = require('mocha')
+
 const chai = require('chai')
-const expect = chai.expect
 chai.use(require('chai-string'))
+
+const R = require('ramda')
+
+module.exports.describe = mocha.describe
+module.exports.it = mocha.it
+module.exports.before = mocha.before
+module.exports.after = mocha.after
+module.exports.beforeEach = mocha.beforeEach
+module.exports.afterEach = mocha.afterEach
+module.exports.expect = mocha.expect
+module.exports.R = mocha.R
